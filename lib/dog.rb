e51aa4ce042b56412e0e1a13945d6eb2b2392c5f
Adam Walker <adamw207@yahoo.com>
2020-11-07 18:33:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  def name=(dogs_name) 
+    @this_dogs_name = dogs_name 
+  end 
+  def name 
+    @this_dogs_name 
+  end 
+end 
+
+fido = DOg.new 
+fido.name = "Fido" 
+
+fido.name 
+fido.bark
+woof! 
