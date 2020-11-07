3de1457d63cedb11946f760b7cfa7c9a108ca812
Adam Walker <adamw207@yahoo.com>
2020-11-07 18:52:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,15 @@ def name=(dogs_name)
 @this_dogs_name = dogs_name 
 end 
 def name 
-  @this_dogs_name 
+  @this_dogs_name
+end
 def bark=(dog_barks)
   @this_dog_barks)
+end
+def bark
+  @this_dog_barks
 end 
-end 
+end
 
 fido = Dog.new 
 fido.name = "Fido"
